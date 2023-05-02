12d4bc723e400e9ca2efd6969981cbb1cbfb1ed4
David H <davidweh7878@hotmail.com>
2023-05-02 16:58:12 -0500

    Second Commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
+import { Routes, Route } from 'react-router-dom';
+import ContactPage from './pages/ContactPage';
+import HomePage from './pages/HomePage';
 import React from 'react';
-import { Container, Navbar, NavbarBrand } from'reactstrap';
-import NucampLogo from './app/assets/img/logo.png';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
+import CampsiteDetailPage from './pages/CampsiteDetailPage';
+import AboutPage from './pages/AboutPage';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-        <Navbar dark color="primary" sticky="top" expand="md">
-            <Container>
-              <NavbarBrand href="/">
-                 <img src={NucampLogo} alt="Nucamp Logo" />
-              </NavbarBrand>
-            </Container>
-        </Navbar>
-        I'm ready for workshop!
+        <Header />
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='contact' element={<ContactPage />} />
+          <Route path='directory' element={<CampsitesDirectoryPage />} />
+          <Route
+            path='directory/:campsiteId'
+            element={<CampsiteDetailPage />}
+          />
+          <Route path='about' element={<AboutPage />} />
+        </Routes>
+        <Footer />
     </div>
   );
 }
